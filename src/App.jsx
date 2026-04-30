871825ce8baf458d285cdf7d92f82b12672320fa
joy <joydot17@gmail.com>
2026-04-30 07:25:37 +0530

    responsive added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Todo from './components/Todo'
 
 const App = () => {
   return (
-    <div className='bg-yellow-50 min-h-screen grid py-5'>
- 
-      <Todo/>
-   </div>
+    <div className='bg-yellow-50 min-h-screen grid py-3 sm:py-5 md:py-10 px-2 sm:px-4 md:px-10'>
+  <Todo/>
+</div>
   )
 }
 
